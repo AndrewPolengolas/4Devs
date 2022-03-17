968491e24b0ddc9fb6102fae2d548a29ac58272c
AndrewPolengolas <andrewnd2009@outlook.com>
2022-03-17 20:09:23 -0300

    adição do spring security

--- src/main/java/com/dev/config/SecurityConfig.java
+++ src/main/java/com/dev/config/SecurityConfig.java
@@ -1,16 +1,21 @@
 package com.dev.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private static final String[] CHAMADAS_PUBLICAS = {
-			"/four-dev.herokuapp.com/**"
+			"/four-dev.herokuapp.com/**",
+			"/users/**"
 	};
 	
 	@Override
@@ -18,6 +23,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests()
 			.antMatchers(CHAMADAS_PUBLICAS).permitAll()
 			.anyRequest().authenticated();
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return (CorsConfigurationSource) source;
 	}
 	
 }
