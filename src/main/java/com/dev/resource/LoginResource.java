b430a20ffec2a922fc67439b1cff789dcc0181a5
AndrewPolengolas <andrewnd2009@outlook.com>
2022-03-23 19:57:01 -0300

    cross

--- src/main/java/com/dev/resource/LoginResource.java
+++ src/main/java/com/dev/resource/LoginResource.java
@@ -2,6 +2,7 @@ package com.dev.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import com.dev.service.LoginService;
 
 @RestController
 @RequestMapping(value="/login")
+@CrossOrigin
 public class LoginResource {
 
 	@Autowired
