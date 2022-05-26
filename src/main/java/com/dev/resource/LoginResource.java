840bd2e0355215a55c23042ab5f0207ad0cdf7ab
AndrewPolengolas <andrewnd2009@outlook.com>
2022-05-26 19:38:49 -0300

    testes

--- src/main/java/com/dev/resource/LoginResource.java
+++ src/main/java/com/dev/resource/LoginResource.java
@@ -3,10 +3,12 @@ package com.dev.resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.dev.entidades.Login;
 import com.dev.service.LoginService;
@@ -21,10 +23,14 @@ public class LoginResource {
 	
 	
 	@PostMapping(value = "/findLogin")
-	public ResponseEntity<Login> buscarLogin(@RequestBody Login obj){
+	public ModelAndView buscarLogin(@ModelAttribute("login") Login obj,
+			RedirectAttributes redirAttr){
 		
 		Login login = loginService.buscarLogin(obj);
 		
-		return ResponseEntity.ok().body(login);
+		ModelAndView mv = new ModelAndView("redirect:/exemplo-view-obj");
+
+		redirAttr.addFlashAttribute("login", login);
+		return mv;
 	}
 }
